812450324b3b3431efac011c6531ea43d38e435c
wesleyskap <wesleyskap@gmail.com>
2025-09-09 10:15:30 -0300

    feat: define ParquetAnalyzerError base exception class

---
+++ src/errors.ts
@@ -0,0 +1,6 @@
+﻿export class ParquetAnalyzerError extends Error {
+  constructor(message: string) {
+    super(`ancalagon-parquet-analyzer: ${message}`);
+    this.name = "ParquetAnalyzerError";
+  }
+}
