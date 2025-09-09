adfcda7b2777b9af6ef130d56c361eca0d491a8e
wesleyskap <wesleyskap@gmail.com>
2025-09-09 10:15:30 -0300

    feat: define ParquetAnalyzerError base exception class

---
+++ src/errors.ts
@@ -0,0 +1,20 @@
+export class ParquetAnalyzerError extends Error {
+  public readonly code: string;
+  constructor(message: string, code: string = "ERR_PARQUET_ANALYZER") {
+    super(message);
+    this.name = "ParquetAnalyzerError";
+    this.code = code;
+  }
+}
+
+export class InvalidHeaderError extends ParquetAnalyzerError {
+  constructor(header: string) {
+    super(`Invalid Parquet header magic bytes: expected 'PAR1', received '${header}'`, "ERR_INVALID_HEADER");
+  }
+}
+
+export class CorruptedFooterError extends ParquetAnalyzerError {
+  constructor(reason: string) {
+    super(`Corrupted Parquet footer metadata: ${reason}`, "ERR_CORRUPTED_FOOTER");
+  }
+}
