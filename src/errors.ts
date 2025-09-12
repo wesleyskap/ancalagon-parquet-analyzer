3d660cc9a1d1f4d87bbc3539b813e47614196bff
wesleyskap <wesleyskap@gmail.com>
2025-09-12 14:09:44 -0300

    feat: add InvalidHeaderError and CorruptedFooterError types

--- src/errors.ts
+++ src/errors.ts
@@ -4,3 +4,17 @@
     this.name = "ParquetAnalyzerError";
   }
 }
+
+export class InvalidHeaderError extends ParquetAnalyzerError {
+  constructor(magic: string) {
+    super(`invalid magic header bytes ${magic}, expected 'PAR1'`);
+    this.name = "InvalidHeaderError";
+  }
+}
+
+export class CorruptedFooterError extends ParquetAnalyzerError {
+  constructor(message: string) {
+    super(`corrupted metadata footer: ${message}`);
+    this.name = "CorruptedFooterError";
+  }
+}
