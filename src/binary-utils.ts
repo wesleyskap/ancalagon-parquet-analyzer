59249d4924a16f5397060847932ba41d103bb404
wesleyskap <wesleyskap@gmail.com>
2025-09-25 09:10:45 -0300

    feat: implement binary offset and size calculator

---
+++ src/binary-utils.ts
@@ -0,0 +1,9 @@
+﻿export class BinaryUtils {
+  public static readInt32LE(buffer: Buffer, offset: number): number {
+    if (offset < 0 || offset + 4 > buffer.length) {
+      return 0;
+    }
+    return buffer.readInt32LE(offset);
+  }
+}
+
