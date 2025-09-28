6ea54d0a68198b5653f161c96e5aca99bc88309d
wesleyskap <wesleyskap@gmail.com>
2025-09-28 15:33:12 -0300

    test: add test cases for binary offset helper

---
+++ test/binary-utils.test.ts
@@ -0,0 +1,10 @@
+﻿import test from "node:test";
+import assert from "node:assert";
+import { BinaryUtils } from "../src/binary-utils.js";
+
+test("BinaryUtils should read Int32LE correctly", () => {
+  const buf = Buffer.alloc(4);
+  buf.writeInt32LE(12345, 0);
+  assert.strictEqual(BinaryUtils.readInt32LE(buf, 0), 12345);
+});
+
