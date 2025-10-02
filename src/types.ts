6a47dd7938ece4e01c361718c4e8cf8eae4b2d3d
wesleyskap <wesleyskap@gmail.com>
2025-10-02 14:15:50 -0300

    feat: define ParquetSchemaField and FileMetadataReport interfaces

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+﻿export interface SchemaFieldReport {
+  name: string;
+  type: string;
+  repetitionType: "REQUIRED" | "OPTIONAL" | "REPEATED";
+}
+
+export interface ColumnReport {
+  name: string;
+  uncompressedSizeBytes: number;
+  compressedSizeBytes: number;
+  compressionRatio: number;
+  encodings: string[];
+}
