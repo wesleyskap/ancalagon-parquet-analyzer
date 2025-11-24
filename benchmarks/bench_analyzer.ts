a7a7d1f13ac6615bb6d916ac318d4f0e8ed89a14
wesleyskap <wesleyskap@gmail.com>
2025-11-24 14:10:00 -0300

    perf: create benchmark suite for parquet metadata inspection

---
+++ benchmarks/bench_analyzer.ts
@@ -0,0 +1,8 @@
+﻿import { ParquetAnalyzer } from "../src/index.js";
+
+function runBenchmark() {
+  const analyzer = new ParquetAnalyzer();
+  const mockBuffer = Buffer.concat([Buffer.from("PAR1"), Buffer.alloc(100), Buffer.from("PAR1")]);
+  analyzer.analyzeBuffer(mockBuffer);
+}
+runBenchmark();
